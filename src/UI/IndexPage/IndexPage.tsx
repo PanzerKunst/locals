3ca360f63b0be064d71dae21cc47e34a4f9265a1
Posidonius <cbramdit@gmail.com>
2023-11-26 13:50:07 +0100

    WIP

--- src/UI/IndexPage/IndexPage.tsx
+++ src/UI/IndexPage/IndexPage.tsx
@@ -31,19 +31,19 @@ export function IndexPage() {
 
       <IndexPageHero/>
 
-      <section id="#for-artists">
+      <section id="for-artists">
         <div className="container">
-          <FadeIn>
+          <FadeIn className="centered-contents">
             <h2>For artists</h2>
           </FadeIn>
 
           <FadeIn>
             <p>Is your primary audience on Spotify?</p>
+          </FadeIn>
 
-            <p>Social media leads to short form, superficial content, read in a few seconds before being scrolled away. This system prevents artists
-              from providing us with high quality, deeper content.</p>
-
-            <p>We listen to a massive variety of artists over time. Manually following every one of them on social media isn&apos;t sustainable.</p>
+          <FadeIn>
+            <p>Social media leads to short form, superficial content, read in a few seconds before being scrolled away. This system prevents you from
+              providing your fans with high quality, deeper content.</p>
           </FadeIn>
 
           <FadeIn className="centered-contents">
@@ -54,19 +54,23 @@ export function IndexPage() {
         </div>
       </section>
 
-      <section id="#for-fans">
+      <section id="for-fans">
         <div className="container">
           <FadeIn>
             <h2>For fans</h2>
           </FadeIn>
 
           <FadeIn>
-            <p>Is your primary audience on Spotify?</p>
+            <p>Our music taste evolves over time, as we constantly discover new music… on Spotify. Backstage Pass leverages your Spotify data to
+              follow you on your listening adventures.</p>
+          </FadeIn>
 
-            <p>Social media leads to short form, superficial content, read in a few seconds before being scrolled away. This system prevents artists
-              from providing us with high quality, deeper content.</p>
+          <FadeIn>
+            <p>We listen to a massive variety of artists over time. Manually following every one of them on social media is less than ideal.</p>
+          </FadeIn>
 
-            <p>We listen to a massive variety of artists over time. Manually following every one of them on social media isn&apos;t sustainable.</p>
+          <FadeIn>
+            <p>It&apos;s about creating a deeper connection with the artists who create the soundtrack to our daily life.</p>
           </FadeIn>
 
           <FadeIn className="centered-contents">
