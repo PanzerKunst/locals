9ec6af7e5b7771ded5b7f4bbe9b3743ccbd9f8c0
Posidonius <cbramdit@gmail.com>
2024-01-04 16:09:03 +0100

    WIP

--- src/UI/_CommonComponents/AppHeader/AppHeader.tsx
+++ src/UI/_CommonComponents/AppHeader/AppHeader.tsx
@@ -1,21 +1,27 @@
 import { useEffect, useMemo, useRef, useState } from "react"
 import { Link, useLocation } from "react-router-dom"
+import classNames from "classnames"
 
 import { AppMenu } from "./AppMenu.tsx"
 import { useAppContext } from "../../../AppContext.tsx"
-
-import classNames from "classnames"
+import { useViewportSize } from "../../../Util/BrowserUtils.ts"
 
 import s from "/src/UI/_CommonStyles/_exports.module.scss"
 import "./AppHeader.scss"
 
-const headerHeight = parseInt(s.headerHeight!)
+const headerHeightMobile = parseInt(s.headerHeightMobile!)
+const headerHeightDesktop = parseInt(s.headerHeightDesktop!)
 const heroPictureMinHeight = parseInt(s.heroPictureMinHeight!)
 let lastScrollY = window.scrollY
 
 export function AppHeader() {
   const { loggedInUser } = useAppContext()
   const location = useLocation()
+
+  const viewportWidth = useViewportSize().width
+  const viewportWidthMd = parseInt(s.vwMd || "")
+  const isMobile = viewportWidth < viewportWidthMd
+
   const headerRef = useRef<HTMLHeadingElement>(null)
   const [isDarkBg, setIsDarkBg] = useState(false)
 
@@ -32,7 +38,40 @@ export function AppHeader() {
       return
     }
 
-    function handleScroll() {
+    const headerHeight = isMobile ? headerHeightMobile : headerHeightDesktop
+    const handleScroll = isMobile ? handleScrollMobile : handleScrollDesktop
+
+    function handleScrollMobile() {
+      const currentScrollY = window.scrollY
+      const delta = currentScrollY - lastScrollY
+      const headerBottomPos = parseInt(header!.style.bottom)
+
+      // Header always shown when scrolled near the top
+      let newBottomPos = 0
+
+      if (currentScrollY > 100) {
+        if (currentScrollY > lastScrollY) { // Scrolling down
+          const isHeaderFullyHidden = headerBottomPos === -headerHeight
+
+          newBottomPos = isHeaderFullyHidden
+            ? -headerHeight
+            : Math.max(headerBottomPos - delta, -headerHeight)
+
+        } else { // Scrolling up
+          const isHeaderFullyVisible = headerBottomPos === 0
+
+          newBottomPos = isHeaderFullyVisible
+            ? 0
+            : Math.min(headerBottomPos - delta, 0)
+        }
+      }
+
+      header!.style.bottom = `${newBottomPos}px`
+
+      lastScrollY = currentScrollY
+    }
+
+    function handleScrollDesktop() {
       const currentScrollY = window.scrollY
       const delta = currentScrollY - lastScrollY
       const headerTopPos = parseInt(header!.style.top)
@@ -69,15 +108,18 @@ export function AppHeader() {
     window.addEventListener("scroll", handleScroll, { passive: true })
 
     return () => window.removeEventListener("scroll", handleScroll)
-  }, [isHeroPicture])
+  }, [isHeroPicture, isMobile])
 
   return (
-    <header ref={headerRef} className={classNames("app-header", { dark: isDarkBg })} style={{ top: 0 }}>
-      <Link to={loggedInUser ? "/home" : "/"} className="button icon-only" aria-label="home">
-        <img src="/images/icon.svg" alt="logo"/>
-      </Link>
-      {loggedInUser ? <AppMenu/> :
-        <Link to="/home" className="underlined appears"><span>Sign in</span></Link>
+    <header
+      ref={headerRef}
+      className={classNames("app-header", { desktop: !isMobile, mobile: isMobile, dark: isDarkBg })}
+      style={isMobile ? { bottom: 0 } : { top: 0 }}
+    >
+      <span/>
+      {loggedInUser
+        ? <AppMenu/>
+        : <Link to="/home" className="underlined appears"><span>Sign in</span></Link>
       }
     </header>
   )
