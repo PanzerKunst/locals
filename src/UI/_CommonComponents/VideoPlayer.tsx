33dd719e8fc370d96848fab67575e184b3d820b4
Posidonius <cbramdit@gmail.com>
2024-01-11 11:03:01 +0100

    WIP

--- src/UI/_CommonComponents/VideoPlayer.tsx
+++ src/UI/_CommonComponents/VideoPlayer.tsx
@@ -1,5 +1,7 @@
 import ReactPlayer from "react-player/lazy"
 
+import { config } from "../../config.ts"
+
 import "./VideoPlayer.scss"
 
 type Props = {
@@ -9,13 +11,27 @@ type Props = {
 export function VideoPlayer({ url }: Props) {
   return (
     <div className="video-player">
-      <ReactPlayer
-        url={url}
-        controls
-        playsInline
-        width="100%"
-        height="100%"
-      />
+      {url.startsWith(config.BACKEND_URL) ? (
+        <div style={{ width: "100%", height: "100%" }}>
+          {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+          <video
+            src={url}
+            controls
+            playsInline
+            width="100%"
+            height="100%"
+          >
+            Your browser does not support HTML5 video.
+          </video>
+        </div>
+      ) : (
+        <ReactPlayer
+          url={url}
+          controls
+          width="100%"
+          height="100%"
+        />
+      )}
     </div>
   )
 }
