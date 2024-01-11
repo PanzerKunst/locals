3a1c4fa010601988d37956fd44994f06dd04b9ff
Posidonius <cbramdit@gmail.com>
2024-01-11 10:10:13 +0100

    WIP

--- src/UI/_CommonComponents/VideoPlayer.tsx
+++ src/UI/_CommonComponents/VideoPlayer.tsx
@@ -1,5 +1,7 @@
 import ReactPlayer from "react-player/lazy"
 
+import { config } from "../../config.ts"
+
 import "./VideoPlayer.scss"
 
 type Props = {
@@ -9,12 +11,24 @@ type Props = {
 export function VideoPlayer({ url }: Props) {
   return (
     <div className="video-player">
-      <ReactPlayer
-        url={url}
-        controls
-        width="100%"
-        height="100%"
-      />
+      {url.startsWith(config.BACKEND_URL) ? (
+        <div style={{ width: "100%", height: "100%"}}>
+          {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+          <video
+            src={url}
+            controls
+            playsInline
+            style={{ width: "100%", height: "100%"}}
+          />
+        </div>
+      ) : (
+        <ReactPlayer
+          url={url}
+          controls
+          width="100%"
+          height="100%"
+        />
+      )}
     </div>
   )
 }
