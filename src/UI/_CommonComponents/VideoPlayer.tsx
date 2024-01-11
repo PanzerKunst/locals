48dd85f7a93a643aadb7c207d5b5dc590520834e
Posidonius <cbramdit@gmail.com>
2024-01-11 11:08:40 +0100

    WIP

--- src/UI/_CommonComponents/VideoPlayer.tsx
+++ src/UI/_CommonComponents/VideoPlayer.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames"
 import ReactPlayer from "react-player/lazy"
 
 import { config } from "../../config.ts"
@@ -9,17 +10,17 @@ type Props = {
 }
 
 export function VideoPlayer({ url }: Props) {
+  const isHosted = url.startsWith(config.BACKEND_URL)
+
   return (
-    <div className="video-player">
-      {url.startsWith(config.BACKEND_URL) ? (
-        <div style={{ width: "100%", height: "100%" }}>
+    <div className={classNames("video-player", { hosted: isHosted })}>
+      {isHosted ? (
+        <div>
           {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
           <video
             src={url}
             controls
             playsInline
-            width="100%"
-            height="100%"
           >
             Your browser does not support HTML5 video.
           </video>
